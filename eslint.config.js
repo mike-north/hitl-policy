0e4fb6d096510052a2d06bce3bd6cf485cc6e359
Mike North <michael.l.north@gmail.com>
2026-08-27 12:44:06 -0700

    Add pre-1.0 release governance

--- eslint.config.js
+++ eslint.config.js
@@ -41,5 +41,16 @@ export default defineConfig(
       '@typescript-eslint/require-await': 'off',
     },
   },
+  {
+    files: ['scripts/*.mjs'],
+    rules: {
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'no-undef': 'off',
+    },
+  },
   prettierConfig,
 );
